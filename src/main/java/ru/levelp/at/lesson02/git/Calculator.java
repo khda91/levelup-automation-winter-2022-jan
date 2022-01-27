409fb54ca5e972debc1019cb26a69ec51e401380
Dmitry Khodakovsky <khda91@gmail.com>
2022-01-27 19:50:23 +0300

    Sum and Subtract methods for calculator

---
+++ src/main/java/ru/levelp/at/lesson02/git/Calculator.java
@@ -0,0 +1,12 @@
+package ru.levelp.at.lesson02.git;
+
+public class Calculator {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
