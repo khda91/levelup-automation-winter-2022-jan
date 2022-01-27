8d6752cb592085ac2f086ce73492f2fc3009acdf
Dmitry Khodakovsky <khda91@gmail.com>
2022-01-27 20:03:11 +0300

    add check b to 0 in divide method

--- src/main/java/ru/levelp/at/lesson02/git/Calculator.java
+++ src/main/java/ru/levelp/at/lesson02/git/Calculator.java
@@ -11,6 +11,9 @@ public class Calculator {
     }
 
     public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("b cannot be 0.");
+        }
         return a / b;
     }
 }
