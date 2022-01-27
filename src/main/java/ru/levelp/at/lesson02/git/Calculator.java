4aee0ee02770852c8a6a8c5ed7986e53184252d2
Dmitry Khodakovsky <khda91@gmail.com>
2022-01-27 20:29:16 +0300

    added factorial and updated divide

--- src/main/java/ru/levelp/at/lesson02/git/Calculator.java
+++ src/main/java/ru/levelp/at/lesson02/git/Calculator.java
@@ -10,10 +10,15 @@ public class Calculator {
         return a - b;
     }
 
-    public int divide(int a, int b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("b cannot be 0.");
-        }
+    public double divide(double a, double b) {
         return a / b;
+    }
+
+    public long factorial(long a) {
+        long result = 1;
+        for (int i = 1; i <= a; i++) {
+            result *= i;
+        }
+        return result;
     }
 }
