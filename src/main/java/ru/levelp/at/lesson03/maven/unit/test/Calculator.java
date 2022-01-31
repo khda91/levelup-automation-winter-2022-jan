3f2e1953252839fac6261f53795c167e0e931d1e
Dmitry Khodakovsky <khda91@gmail.com>
2022-01-31 22:04:29 +0300

    Lesson 03 - Build & unit testing tools
    * maven
    * gradle
    * TestNg

---
+++ src/main/java/ru/levelp/at/lesson03/maven/unit/test/Calculator.java
@@ -0,0 +1,67 @@
+package ru.levelp.at.lesson03.maven.unit.test;
+
+public class Calculator {
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
+
+    public int power(int a, int b) {
+        if (b < 0) {
+            throw new IllegalArgumentException("b cannot be negative.");
+        }
+
+        int result = 1;
+
+        for (int i = 0; i < b; i++) {
+            result *= a;
+        }
+        return result;
+    }
+
+    public long factorial(long a) {
+        long result = 1;
+        for (int i = 1; i <= a; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public double cos(double a) {
+        return Math.cos(a);
+    }
+
+    public double tanh(double a) {
+        return Math.tanh(a);
+    }
+
+    public int abs(int a) {
+        if (a < 0) {
+            return a * -1;
+        }
+        return a;
+    }
+
+    public long abs(long a) {
+        if (a < 0) {
+            return a * -1;
+        }
+        return a;
+    }
+
+    public double abs(double a) {
+        if (a < 0) {
+            return a * -1;
+        }
+        return a;
+    }
+
+}
---
+++ src/test/java/ru/levelp/at/lesson03/maven/unit/test/SampleTestClass.java
@@ -0,0 +1,11 @@
+package ru.levelp.at.lesson03.maven.unit.test;
+
+import org.testng.annotations.Test;
+
+public class SampleTestClass {
+
+    @Test
+    public void test() {
+
+    }
+}
